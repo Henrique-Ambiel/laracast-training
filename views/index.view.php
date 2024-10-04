cdddc6deed49aa6bc9dfbd6296e79bab844cecd0
Henrique <henriqueambiel@gmail.com>
2024-10-04 16:09:14 -0300

    Commit

--- views/index.view.php
+++ views/index.view.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html lang="en" class= "h-full bg-gray-100">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Laracast Training</title>
-  <script src="https://cdn.tailwindcss.com"></script>
-</head>
-<body class= "h-full">
-<div class="min-h-full">
+<?php require('partials/head.php');?>
  
 <?php require('partials/nav.php');?>
 
@@ -21,7 +12,5 @@
       <p> Hello! Welcome to our website. </p>
     </div>
   </main>
-</div>
-
-</body>
-</html>
+  
+<?php require('partials/footer.php');?>
