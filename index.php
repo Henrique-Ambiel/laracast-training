febf55538f7c0515a20c88a2265d3b32aab21cdf
Henrique <henriqueambiel@gmail.com>
2024-10-04 11:49:51 -0300

    Update index.php

--- index.php
+++ index.php
@@ -2,10 +2,19 @@
 
 require 'functions.php';
 
-$uri = $_SERVER[REQUEST_URI];
+$uri = parse_url($_SERVER[REQUEST_URI])['path']; /*parse_url() analisa uma URL e retorna seus componentes 
+como um array associativo*/
 
-if ($uri == '/'){
-    require 'controllers/index.php';
-} else if($uri == '/about'){
-    require 'controllers/about.php';
+$routes = [
+    '/' => 'controllers/index.php',
+    '/about' => 'controllers/about.php',
+    '/contact' => 'controlers/contact.php',
+];
+
+if(array_key_exists($uri, $routes)){ //Verifica se uma chave específica existe dentro de um array
+    require $routes[$uri];
+} else {
+    http_response_code(404); //Define o código de status HTTP que o servidor retornará ao cliente
+
+    echo 'Sorry, not found';
 }
